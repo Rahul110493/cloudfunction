962927a17de134b5a8087d756dcc8bf80e947bf6
Rahul110493 <38890875+Rahul110493@users.noreply.github.com>
2021-10-12 13:54:45 +0530

    Create main.tf

---
+++ tf-scripts/gcp/service/cloudscheduler/main.tf
@@ -0,0 +1,10 @@
+resource "google_cloud_scheduler_job" "cloudscheduler" {
+  name         = var.scheduler_name
+  description  = "cloudscheduler"
+  schedule     = var.scheduler_cron
+  time_zone    = var.scheduler_time_zone
+  
+  http_target {
+    http_method = "GET"
+    uri = google_cloudfunctions_function.function.https_trigger_url
+}
