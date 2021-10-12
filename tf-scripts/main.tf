357bd4d0e6492964ece91514d8a0f3e95fcbf0b7
Rahul110493 <38890875+Rahul110493@users.noreply.github.com>
2021-10-12 13:52:52 +0530

    Update main.tf

--- tf-scripts/main.tf
+++ tf-scripts/main.tf
@@ -1,1 +1,32 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.82.0"
+    }
+  }
+}
+provider "google" {
+  project = "${var.project_id}"
+  region  = "${var.region}"
+  zone = "${var.zone}"
+}
 
+module "cloudscheduler" {
+    count = var.vendor == "cloudscheduler" ? 1 : 0
+    source = "./gcp/service/cloudscheduler"
+
+    scheduler_name = var.scheduler_name
+    scheduler_cron = var.scheduler_cron
+    scheduler_time_zone = var.scheduler_time_zone   
+}
+
+module "cloudscheduler_data" {
+    count = var.vendor == "cloudscheduler_data" ? 1 : 0
+    source = "./gcp/service/cloudscheduler_data"
+
+    scheduler_name = var.scheduler_name
+    scheduler_cron = var.scheduler_cron
+    scheduler_time_zone = var.scheduler_time_zone  
+    body = var.body
+}
