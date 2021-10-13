4776cb326688c5eb6e2fc2ea0343649dd71d72df
Rahul110493 <38890875+Rahul110493@users.noreply.github.com>
2021-10-13 14:56:30 +0530

    Update output.tf

--- tf-scripts/gcp/service/cloudfunctions/output.tf
+++ tf-scripts/gcp/service/cloudfunctions/output.tf
@@ -1,4 +1,4 @@
 output "url" {
  description = "url"
- value = google_cloudfunctions_function.function.https_trigger_url/api/portfolio
+ value = google_cloudfunctions_function.function.https_trigger_url+"/api/portfolio"
 }
