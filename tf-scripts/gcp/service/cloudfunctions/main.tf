830b34223b8dfdf7377dad0532c12d0420cbeb9e
Rahul110493 <38890875+Rahul110493@users.noreply.github.com>
2021-10-21 15:17:20 +0530

    Update main.tf

--- tf-scripts/gcp/service/cloudfunctions/main.tf
+++ tf-scripts/gcp/service/cloudfunctions/main.tf
@@ -46,5 +46,5 @@ resource "google_cloudfunctions_function_iam_member" "invoker" {
   cloud_function = google_cloudfunctions_function.function.name
 
   role   = "roles/cloudfunctions.invoker"
-  member = serviceAccount:var.service_account_email
+  member = "serviceAccount:var.service_account_email"
 }
