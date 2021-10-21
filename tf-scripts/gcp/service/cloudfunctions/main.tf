a42222d74148498c54e29756db208cbed478ed22
Rahul110493 <38890875+Rahul110493@users.noreply.github.com>
2021-10-21 16:42:13 +0530

    Update main.tf

--- tf-scripts/gcp/service/cloudfunctions/main.tf
+++ tf-scripts/gcp/service/cloudfunctions/main.tf
@@ -34,7 +34,6 @@ resource "google_cloudfunctions_function" "function" {
   entry_point           = var.entry_point
   runtime               = var.runtime
   environment_variables = var.environment_variables
-  service_account_email = var.service_account_email
   vpc_connector         = var.vpc_connector
   max_instances         = var.max_instances
   region                = var.region
