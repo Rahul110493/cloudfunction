719d59973e7b150afe2cbc89d8058cbc5e8a4bcc
Rahul110493 <38890875+Rahul110493@users.noreply.github.com>
2021-10-25 12:21:55 +0530

    Update main.tf

--- tf-scripts/gcp/service/service-account/main.tf
+++ tf-scripts/gcp/service/service-account/main.tf
@@ -1,13 +1,8 @@
-resource "google_service_account" "sa" {
-  account_id   = var.sa_name
-  display_name = "Service account for retention"
-}
-
 resource "google_service_account_iam_binding" "admin-account-iam" {
-  service_account_id = google_service_account.sa.name
+  service_account_id = var.sa_name
   role               = "roles/iam.serviceAccountUser"
 
   members = [
-    var.pipeline_sa
+    var.sa_name
   ]
 }
