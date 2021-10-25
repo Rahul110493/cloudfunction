265cb420ebd4f708794d86018236130254bad714
Rahul110493 <38890875+Rahul110493@users.noreply.github.com>
2021-10-25 12:33:13 +0530

    Update main.tf

--- tf-scripts/gcp/service/service-account/main.tf
+++ tf-scripts/gcp/service/service-account/main.tf
@@ -1,5 +1,5 @@
 resource "google_service_account_iam_binding" "admin-account-iam" {
-  service_account_id = var.sa_name
+  service_account_id = "103074584374432099583"
   role               = "roles/iam.serviceAccountUser"
 
   members = [
